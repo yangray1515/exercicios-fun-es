4d9fa381686cd74eac4eef0b3109f0c916018e2c
yangray1515 <102757737+yangray1515@users.noreply.github.com>
2022-05-05 15:42:43 -0300

    Add files via upload

---
+++ exerfunçoes.js
@@ -0,0 +1,24 @@
+function nome (nome){
+    console.log(nome)
+    return
+}
+nome('Raiana')
+
+function soma(a,b){
+    console.log(a+b)
+    return
+}
+soma(5,5)
+
+function multiplicacao(c,d){
+console.log(c*d)
+return
+}
+multiplicacao(4,3)
+
+function media(e,f,g,h){
+    let res=(e+f+g+h)
+    console.log(res/4)
+    return
+}
+media(6,8,10,8)
